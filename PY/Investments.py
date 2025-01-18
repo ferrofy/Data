75c8f3f5fe612a7b9b700f38850f2df50d29731b
ferrofy <team.ferrofy@gmail.com>
2025-01-18 12:56:43 +0530

    Update Investments.py

--- PY/Investments.py
+++ PY/Investments.py
@@ -42,7 +42,7 @@ Live_NFT__Investments = sum(Investments_NFT__0xt)
 
 # ________________Total VPX__________________
 VPX_Total_Investments = Investments_Pool_0xt[0] + Investments_Farm_0xt[0] + Investments_Brow_0xt[0] + Investments_NFT__0xt[0] + Investments_Pool_0x0_1[0] + Investments_Farm_0x0_1[0] + Investments_Brow_0x0_1[0] + Investments_NFT__0x0_1[0]
-VPX_Total_Live_Investments = Investments_Farm_0xt[0] + Investments_Brow_0xt[0] + Investments_NFT__0xt[0] 
+VPX_Total_Live_Investments = Investments_Farm_0xt[0] + Investments_Brow_0xt[0] + Investments_NFT__0xt[0] #Live Investments May Changed According To Situation :)
 VPX_Total_Pool_Investments = Investments_Pool_0xt[0] + Investments_Pool_0x0_1[0]
 VPX_Total_Farm_Investments = Investments_Farm_0xt[0] + Investments_Farm_0x0_1[0]
 VPX_Total_Brow_Investments = Investments_Brow_0xt[0] + Investments_Brow_0x0_1[0]
@@ -50,7 +50,7 @@ VPX_Total_NFT__Investments = Investments_NFT__0xt[0] + Investments_NFT__0x0_1[0]
 
 # _______________Total Tanav_________________
 Tanav_Total_Investments = Investments_Pool_0xt[1] + Investments_Farm_0xt[1] + Investments_Brow_0xt[1] + Investments_NFT__0xt[1] + Investments_Pool_0x0_1[1] + Investments_Farm_0x0_1[1] + Investments_Brow_0x0_1[1] + Investments_NFT__0x0_1[1]
-Tanav_Total_Live_Investments = Investments_Farm_0xt[1] + Investments_Brow_0xt[1] + Investments_NFT__0xt[1] 
+Tanav_Total_Live_Investments = Investments_Farm_0xt[1] + Investments_Brow_0xt[1] + Investments_NFT__0xt[1] #Live Investments May Changed According To Situation :)
 Tanav_Total_Pool_Investments = Investments_Pool_0xt[1] + Investments_Pool_0x0_1[1]
 Tanav_Total_Farm_Investments = Investments_Farm_0xt[1] + Investments_Farm_0x0_1[1]
 Tanav_Total_Brow_Investments = Investments_Brow_0xt[1] + Investments_Brow_0x0_1[1]
@@ -58,7 +58,7 @@ Tanav_Total_NFT__Investments = Investments_NFT__0xt[1] + Investments_NFT__0x0_1[
 
 # ________________Total KMX__________________
 KMX_Total_Investments = Investments_Pool_0xt[2] + Investments_Farm_0xt[2] + Investments_Brow_0xt[2] + Investments_NFT__0xt[2] + Investments_Pool_0x0_1[2] + Investments_Farm_0x0_1[2] + Investments_Brow_0x0_1[2] + Investments_NFT__0x0_1[2]
-KMX_Total_Live_Investments = Investments_Farm_0xt[2] + Investments_Brow_0xt[2] + Investments_NFT__0xt[2] 
+KMX_Total_Live_Investments = Investments_Farm_0xt[2] + Investments_Brow_0xt[2] + Investments_NFT__0xt[2] #Live Investments May Changed According To Situation :)
 KMX_Total_Pool_Investments = Investments_Pool_0xt[2] + Investments_Pool_0x0_1[2]
 KMX_Total_Farm_Investments = Investments_Farm_0xt[2] + Investments_Farm_0x0_1[2]
 KMX_Total_Brow_Investments = Investments_Brow_0xt[2] + Investments_Brow_0x0_1[2]
@@ -66,7 +66,7 @@ KMX_Total_NFT__Investments = Investments_NFT__0xt[2] + Investments_NFT__0x0_1[2]
 
 # ________________Total AKX__________________
 AKX_Total_Investments = Investments_Pool_0xt[3] + Investments_Farm_0xt[3] + Investments_Brow_0xt[3] + Investments_NFT__0xt[3] + Investments_Pool_0x0_1[3] + Investments_Farm_0x0_1[3] + Investments_Brow_0x0_1[3] + Investments_NFT__0x0_1[3]
-AKX_Total_Live_Investments = Investments_Farm_0xt[3] + Investments_Brow_0xt[3] + Investments_NFT__0xt[3] 
+AKX_Total_Live_Investments = Investments_Farm_0xt[3] + Investments_Brow_0xt[3] + Investments_NFT__0xt[3] #Live Investments May Changed According To Situation :)
 AKX_Total_Pool_Investments = Investments_Pool_0xt[3] + Investments_Pool_0x0_1[3]
 AKX_Total_Farm_Investments = Investments_Farm_0xt[3] + Investments_Farm_0x0_1[3]
 AKX_Total_Brow_Investments = Investments_Brow_0xt[3] + Investments_Brow_0x0_1[3]
@@ -74,7 +74,7 @@ AKX_Total_NFT__Investments = Investments_NFT__0xt[3] + Investments_NFT__0x0_1[3]
 
 # ________________Total Angel__________________
 Angel_Total_Investments = Investments_Pool_0xt[4] + Investments_Farm_0xt[4] + Investments_Brow_0xt[4] + Investments_NFT__0xt[4] + Investments_Pool_0x0_1[4] + Investments_Farm_0x0_1[4] + Investments_Brow_0x0_1[4] + Investments_NFT__0x0_1[4]
-Angel_Total_Live_Investments = Investments_Farm_0xt[4] + Investments_Brow_0xt[4] + Investments_NFT__0xt[4] 
+Angel_Total_Live_Investments = Investments_Farm_0xt[4] + Investments_Brow_0xt[4] + Investments_NFT__0xt[4] #Live Investments May Changed According To Situation :) 
 Angel_Total_Pool_Investments = Investments_Pool_0xt[4] + Investments_Pool_0x0_1[4]
 Angel_Total_Farm_Investments = Investments_Farm_0xt[4] + Investments_Farm_0x0_1[4]
 Angel_Total_Brow_Investments = Investments_Brow_0xt[4] + Investments_Brow_0x0_1[4]
@@ -82,7 +82,7 @@ Angel_Total_NFT__Investments = Investments_NFT__0xt[4] + Investments_NFT__0x0_1[
 
 # ________________Total UCI__________________
 UCI_Total_Investments = Investments_Pool_0xt[5] + Investments_Farm_0xt[5] + Investments_Brow_0xt[5] + Investments_NFT__0xt[5] + Investments_Pool_0x0_1[5] + Investments_Farm_0x0_1[5] + Investments_Brow_0x0_1[5] + Investments_NFT__0x0_1[5]
-UCI_Total_Live_Investments = Investments_Farm_0xt[5] + Investments_Brow_0xt[5] + Investments_NFT__0xt[5] 
+UCI_Total_Live_Investments = Investments_Farm_0xt[5] + Investments_Brow_0xt[5] + Investments_NFT__0xt[5] #Live Investments May Changed According To Situation :)
 UCI_Total_Pool_Investments = Investments_Pool_0xt[5] + Investments_Pool_0x0_1[5]
 UCI_Total_Farm_Investments = Investments_Farm_0xt[5] + Investments_Farm_0x0_1[5]
 UCI_Total_Brow_Investments = Investments_Brow_0xt[5] + Investments_Brow_0x0_1[5]
@@ -90,7 +90,7 @@ UCI_Total_NFT__Investments = Investments_NFT__0xt[5] + Investments_NFT__0x0_1[5]
 
 # ________________Total SSX__________________
 SSX_Total_Investments = Investments_Pool_0xt[6] + Investments_Farm_0xt[6] + Investments_Brow_0xt[6] + Investments_NFT__0xt[6] + Investments_Pool_0x0_1[6] + Investments_Farm_0x0_1[6] + Investments_Brow_0x0_1[6] + Investments_NFT__0x0_1[6]
-SSX_Total_Live_Investments = Investments_Farm_0xt[6] + Investments_Brow_0xt[6] + Investments_NFT__0xt[6] 
+SSX_Total_Live_Investments = Investments_Farm_0xt[6] + Investments_Brow_0xt[6] + Investments_NFT__0xt[6] #Live Investments May Changed According To Situation :)
 SSX_Total_Pool_Investments = Investments_Pool_0xt[6] + Investments_Pool_0x0_1[6]
 SSX_Total_Farm_Investments = Investments_Farm_0xt[6] + Investments_Farm_0x0_1[6]
 SSX_Total_Brow_Investments = Investments_Brow_0xt[6] + Investments_Brow_0x0_1[6]
@@ -98,7 +98,7 @@ SSX_Total_NFT__Investments = Investments_NFT__0xt[6] + Investments_NFT__0x0_1[6]
 
 # ________________Total PPX__________________
 PPX_Total_Investments = Investments_Pool_0xt[7] + Investments_Farm_0xt[7] + Investments_Brow_0xt[7] + Investments_NFT__0xt[7] + Investments_Pool_0x0_1[7] + Investments_Farm_0x0_1[7] + Investments_Brow_0x0_1[7] + Investments_NFT__0x0_1[7]
-PPX_Total_Live_Investments = Investments_Farm_0xt[7] + Investments_Brow_0xt[7] + Investments_NFT__0xt[7] 
+PPX_Total_Live_Investments = Investments_Farm_0xt[7] + Investments_Brow_0xt[7] + Investments_NFT__0xt[7] #Live Investments May Changed According To Situation :)
 PPX_Total_Pool_Investments = Investments_Pool_0xt[7] + Investments_Pool_0x0_1[7]
 PPX_Total_Farm_Investments = Investments_Farm_0xt[7] + Investments_Farm_0x0_1[7]
 PPX_Total_Brow_Investments = Investments_Brow_0xt[7] + Investments_Brow_0x0_1[7]
