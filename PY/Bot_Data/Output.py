5a299dc3a2d8ebcae8121015ba8e48309809b28a
ferrofy <team.ferrofy@gmail.com>
2025-01-20 01:56:38 +0530

    Create Output.py

---
+++ PY/Bot_Data/Output.py
@@ -0,0 +1,17 @@
+FerroFy_RuleBook = """
+________________________________________
+      FerroFy RuleBook / InfoBook
+________________________________________
+
+>>> Gmails With '0xt' Are Test Phase Gmail & Gmail is 'heat.free.crypto@gmail.com'
+>>> All Companys Shares Distrubution ~ Investors ==> 75% , FerroFy ==> 25%
+>>> FerroFy Shares Distribution ~ Investors ==> 75% & Public ==> 25%
+>>> More Rules Soon
+
+
+
+>>> Note: Final Fixed Shares Will Be Distributed Later For More Info Contact 'team.ferrofy@gmail.com' or 'vpx.ferrofy@gmail.com'
+>>> InfoBook / RuleBook Is Make By
+>>> @VPX
+>>> Thanks For Your Time :)
+"""
