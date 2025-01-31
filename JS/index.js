a6c60648d0f996d6df79f73e530276ca8deaa77a
ferrofy <team.ferrofy@gmail.com>
2025-01-31 19:24:18 +0530

    Update and rename Btn.js to index.js

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,6 @@ document.getElementById('Go_To_Live_Investments').addEventListener('click', func
 document.getElementById('Go_To_Past_Investments').addEventListener('click', function() {
   window.location.href = 'https://ferrofy.github.io/Data/Past_Investments';
 });
+document.getElementById('Go_To_Shares').addEventListener('click', function() {
+  window.location.href = 'https://ferrofy.github.io/Data/Shares';
+});
