2322791c6194993f249312a80329570f112e229e
ferrofy <team.ferrofy@gmail.com>
2025-02-02 21:23:59 +0530

    Update Shares.js

--- JS/Shares.js
+++ JS/Shares.js
@@ -2,23 +2,19 @@ const planPool0xt = { VPX: 510, Tanav: 610, KMX: 20, AKX: 20, Angel: 0, UCI: 0, 
 const planFarm0xt = { VPX: 460, Tanav: 460, KMX: 0, AKX: 80, Angel: 70, UCI: 0, SSX: 0, PPX: 500 };
 
 const colors = {
-    nameColor: "yellow",
-    ffxTextColor: "#39ff14",
-    ffxLabelColor: "red",
-    backgroundColor: "#1d1f21",
-    textColor: "white",
-    accentColor: "#39ff14",
-    finalTextColor: "cyan",
-    suffixColor: "darkpink",
-    noteColor: "white"
+    tableHeader: "cyan",
+    nameColor: "#ffff00", 
+    sharesColor: "white",
+    multiplierColor: "red",
+    maxSupplyColor: "yellow",
+    laColor: "red",
+    ffxTextColor: "#ffff00", 
+    noteColor: "white",
+    captionColor: "#39ff14"
 };
 
-const Animation_Delay = 15;
-
 const formatNumber = (num) => {
-    if (num >= 10000000) {
-        return (num / 10000000).toFixed(7) + ' Cr';
-    } else if (num >= 100000) {
+    if (num >= 100000) {
         return (num / 100000).toFixed(5) + ' La';
     } else if (num >= 1000) {
         return (num / 1000).toFixed(3) + ' Th';
@@ -28,9 +24,10 @@ const formatNumber = (num) => {
 };
 
 const updateLandingPage = () => {
-    const investmentsContent = document.getElementById('investmentsContent');
+    const sharesTableBody = document.querySelector('#sharesTable tbody');
+    const combinedTableBody = document.querySelector('#combinedTable tbody');
     const totalFerroFyInvestments = calculateTotalInvestments();
-    const FFXRate = 0.000001;
+    const FFXRate = 0.001;
 
     const calculateFFX = (investment) => {
         const share = (investment / totalFerroFyInvestments) * 75;
@@ -39,58 +36,53 @@ const updateLandingPage = () => {
 
     let delay = 0;
 
-    const shareAnimation = [];
-
-    for (let i = 0; i <= 100; i++) {
-        let dots = ''.repeat(i % 3 + 1);
-        let hashes = '#'.repeat(Math.floor(i / 10));
-        let spaces = ' '.repeat(10 - hashes.length);
-        let percentage = `${i}%`;
-
-        shareAnimation.push(`Calculating... ${dots} [${hashes}${spaces}] ${percentage}`);
-    }
-
-    console.log(shareAnimation);
-
-
     for (const person in planPool0xt) {
         setTimeout(() => {
-            investmentsContent.innerHTML += `<p style="color: ${colors.nameColor}" id="${person}">${person}: ${shareAnimation[0]}</p>`;
-        }, delay);
-
-        delay += Animation_Delay;
-
-        for (let i = 1; i < shareAnimation.length; i++) {
-            setTimeout(() => {
-                document.getElementById(person).innerHTML = `${person}: ${shareAnimation[i]}`;
-            }, delay);
-            delay += Animation_Delay;
-        }
-
-        setTimeout(() => {
             const ffxValue = calculateFFX(planPool0xt[person] + planFarm0xt[person]);
-            let suffix = '';
-            if (ffxValue.includes('Cr')) {
-                suffix = 'Cr';
-            } else if (ffxValue.includes('La')) {
-                suffix = 'La';
+            let multiplier = '--';
+            if (ffxValue.includes('La')) {
+                multiplier = 'La';
             } else if (ffxValue.includes('Th')) {
-                suffix = 'Th';
+                multiplier = 'Th';
             }
-            const formattedValue = ffxValue.replace(suffix, '').trim();
-            document.getElementById(person).innerHTML =
-                `<span style="color: ${colors.nameColor}">${person}</span> : 
-                <span style="color: ${colors.textColor}">${formattedValue}</span> 
-                <span style="color: ${colors.ffxLabelColor}">${suffix}</span> 
-                <span style="color: ${colors.ffxTextColor}">FFX</span>`;
-        }, delay);
+            const formattedValue = ffxValue.replace(multiplier, '').trim();
 
-        delay += Animation_Delay;
+            const row = sharesTableBody.insertRow();
+            row.insertCell(0).innerHTML = `<span style="color: ${colors.nameColor}">${person}</span>`;
+            row.insertCell(1).innerHTML = `<span style="color: ${colors.sharesColor}">${formattedValue}</span>`;
+            row.insertCell(2).innerHTML = `<span style="color: ${colors.multiplierColor}">${multiplier}</span>`;
+        }, delay);
     }
 
-    setTimeout(() => {
-        investmentsContent.innerHTML += `<p style="color: ${colors.finalTextColor}">1 FFX = ${FFXRate}% Of FerroFy</p>`;
-    }, delay);
+    const rowsData = [
+        {
+            type: "Circulating Supply",
+            amount: "1",
+            multiplier: "La"
+        },
+        {
+            type: "Max Supply",
+            amount: "--",
+            multiplier: "--"
+        },
+        {
+            type: "Investors",
+            amount: "75",
+            multiplier: "Th"
+        },
+        {
+            type: "Public",
+            amount: "25",
+            multiplier: "Th"
+        }
+    ];
+
+    rowsData.forEach((data) => {
+        const row = combinedTableBody.insertRow();
+        row.insertCell(0).innerHTML = `<span style="color: ${colors.nameColor};">${data.type}</span>`;
+        row.insertCell(1).innerHTML = `<span style="color: ${colors.sharesColor};">${data.amount}</span>`;
+        row.insertCell(2).innerHTML = `<span style="color: ${colors.multiplierColor};">${data.multiplier}</span>`;
+    });
 };
 
 const calculateTotalInvestments = () => {
